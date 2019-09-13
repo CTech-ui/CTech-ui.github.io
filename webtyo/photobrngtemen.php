510060d7b00a0746422cc728b0fd6a61fb26a777
CTech-ui <55191292+CTech-ui@users.noreply.github.com>
2019-09-13 20:24:52 +0700

    Rancangan Web Anime
    Sekalian ini tugas dari guru:D

---
+++ webtyo/photobrngtemen.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css" class="rel">
+
+    <title>biografi</title>
+  </head>
+  <body>
+
+       <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">kemana kita akan pergi?</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+         <a class="nav-link" href="http://localhost/webtyo/">Beranda</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/webtyo/biografi.php">Biografi</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/webtyo/biodata.php">Biodata</a>
+      </li>
+      <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="background: #2196f3">
+          Photo
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="http://localhost/webtyo/photopribadi.php">Photo pribadi</a>
+          <a class="dropdown-item" href="http://localhost/webtyo/photobrngtemen.php">Photo bareng teman</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+    
+   <!-- end of navbar -->
+    
+   <!-- Header -->
+  <div class="jumbotron text-center jumbotron-dark bg-dark" style="padding: 10px 50px">
+    <h1 style="color: #fff">Photo bareng temen</h1>
+    <p style="color: #fff">This is my Photo when i with my friend</p>
+  </div>
+   <!-- end of header -->
+
+      <!-- Content -->
+   <p>
+     
+   </p>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br><br><br><br><br><br>
+   <!-- end of content -->
+
+   <!-- footer -->
+     <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Copyright &copy; 2019 · <a href="#"> CTech-ui Website <br></a>
+        Made by Sublime Text
+      </small>
+    </div>
+  </footer>
+</body>
+   <!-- end of footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
